29b93c3c849457653204f536f92632db1c912807
quol <kuolamol77@gmail.com>
2025-04-30 12:35:39 +0300

    styled the nav menu on small and medium screens

--- script.js
+++ script.js
@@ -2,21 +2,29 @@
 const openBar = document.querySelector('.uil-bars');
 const closeBar = document.querySelector('.uil-times');
 const nav = document.querySelector('.nav-list');
+const navlinks = document.querySelector('.nav-list a');
 
 
 
 openBar.addEventListener('click', () => {
-  nav.style.display = 'block';
-  if (nav.style.display === 'block') {
+  nav.style.display = 'inline-block';
+  // if (nav.style.display === 'block') {
     openBar.style.display = 'none';
-    closeBar.style.display = 'block';
-  }
+    closeBar.style.display = 'inline-block';
+  // }
+  navlinks.forEach(item => {
+    item.addEventListener('click', () => {
+      nav.style.display = 'none';          // Hide the menu
+      openBar.style.display = 'inline-block'; // Show the hamburger icon
+      closeBar.style.display = 'none';      // Hide the close icon
+    });
+  });
 });
 
 closeBar.addEventListener('click', () => {
   nav.style.display = 'none';
-  if (nav.style.display === 'none') {
-    openBar.style.display = 'block';
+  // if (nav.style.display === 'none') {
+    openBar.style.display = 'inline-block';
     closeBar.style.display = 'none';
-  }
+  // }
 });
