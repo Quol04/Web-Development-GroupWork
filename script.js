7a2b6d62ecdfbc4a8706981bcef12298f91df832
quol <kuolamol77@gmail.com>
2025-04-30 13:25:47 +0300

    made the site to be in dark mode whenever it is pressed

--- script.js
+++ script.js
@@ -2,16 +2,15 @@
 const openBar = document.querySelector('.uil-bars');
 const closeBar = document.querySelector('.uil-times');
 const nav = document.querySelector('.nav-list');
-const navlinks = document.querySelector('.nav-list a');
+const navlinks = document.querySelectorAll('.nav-list a');
 
 
 
 openBar.addEventListener('click', () => {
   nav.style.display = 'inline-block';
-  // if (nav.style.display === 'block') {
-    openBar.style.display = 'none';
-    closeBar.style.display = 'inline-block';
-  // }
+  openBar.style.display = 'none';
+  closeBar.style.display = 'inline-block';
+
   navlinks.forEach(item => {
     item.addEventListener('click', () => {
       nav.style.display = 'none';          // Hide the menu
@@ -23,8 +22,28 @@ openBar.addEventListener('click', () => {
 
 closeBar.addEventListener('click', () => {
   nav.style.display = 'none';
-  // if (nav.style.display === 'none') {
-    openBar.style.display = 'inline-block';
-    closeBar.style.display = 'none';
-  // }
+  openBar.style.display = 'inline-block';
+  closeBar.style.display = 'none';
+ 
 });
+
+//dark-mode
+const darkMode = document.querySelector('#dark-mode');
+const lightMode = document.querySelector('#light-mode');
+const body = document.querySelector('body');
+
+
+darkMode.addEventListener('click', () => { 
+  body.classList.toggle('dark-mode');
+  darkMode.style.display = 'none';
+  lightMode.style.display = 'block';
+  
+});
+
+lightMode.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+  lightMode.style.display = 'none';
+  darkMode.style.display = 'block';
+  
+});
+
